503855e1b80474d1985af48168afd49160f0172a
limaarthur <artglima.dev@gmail.com>
2025-04-04 15:57:42 -0300

    first commit

---
+++ src/main/java/com/igniteedcuation/IgniteedcuationApplication.java
@@ -0,0 +1,13 @@
+package com.igniteedcuation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgniteedcuationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgniteedcuationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igniteedcuation/IgniteedcuationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igniteedcuation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgniteedcuationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

957f914fbe5de804306d9cc1306f644d8ec675cf
limaarthur <artglima.dev@gmail.com>
2025-04-05 11:04:36 -0300

    feat: creation user and role entities and repositories

---
+++ src/main/java/com/igniteedcuation/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.igniteedcuation.repositories;
+
+import com.igniteedcuation.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
